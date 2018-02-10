70c8bf972802bc2f9c19be86258427e883159fc0
Stephanie Nemeth <traumverloren@gmail.com>
2018-02-10 19:32:30 +0100

    update huzzah code for new color selector  :sparkles:

--- led-necklace.ino
+++ led-necklace.ino
@@ -1,25 +1,31 @@
 #include <ESP8266WiFi.h>
 #include <Adafruit_NeoPixel.h> // Include the adafruit Neopixel Library
 #include <MQTTClient.h>
-
-const char ssid[] = "ssid";
-const char pass[] = "password";
-
-WiFiClient net;
-MQTTClient client;
-
-unsigned long lastMillis = 0;
-
-void connect();  // <- predefine connect() for setup()
-
+#include "arduino_secrets.h" 
 
 #define PIN         2
 #define LED_COUNT  52
 
-const int brightness = 5;
+const char ssid[] = SECRET_SSID;
+const char pass[] = SECRET_PASS;
 
-enum mode {modeColorWipe, modeRainbowRain, modeRain, modeRainbow, modeRainbowSparkle, modeSnake, modeSparkle};
-mode currentMode = modeRainbowSparkle;
+unsigned long lastMillis = 0;
+const int brightness = 15;
+bool rainbowOn = false;
+int colorR = 0;
+int colorG = 0;
+int colorB = 255;
+uint16_t TotalSteps = 400;  // total number of steps in the pattern
+uint16_t Index;  // current step within the pattern
+uint32_t Color1, Color2;  // What colors are in use (used by Fade)
+
+WiFiClient net;
+MQTTClient client;
+
+void connect();  // <- predefine connect() for setup()
+
+enum mode {modeFade, modeRain, modeSnake, modeSparkle};
+mode currentMode = modeRain;
 
 // Parameter 1 = number of pixels in strip
 // Parameter 2 = pin number (most are valid)
@@ -35,14 +41,11 @@ void setup() {
   rings.begin();
   rings.setBrightness(brightness);
   rings.show();
-  
   Serial.begin(115200);
   WiFi.begin(ssid, pass);
 
   // Note: Local domain names (e.g. "Computer.local" on OSX) are not supported by Arduino.
   // You need to set the IP address directly.
-  //
-  // MQTT brokers usually use port 8883 for secure connections.
   client.begin(IP_ADDRESS, PORT, net);
   client.onMessage(messageReceived);
 
@@ -57,7 +60,7 @@ void connect() {
   }
 
   Serial.print("\nconnecting...");
-  while (!client.connect("led-necklace-huzzah", "key", "token")) {
+  while (!client.connect("led-necklace-m0", "", "")) {
     Serial.print(".");
     delay(1000);
   }
@@ -66,36 +69,68 @@ void connect() {
   client.subscribe("lights");
 }
 
+void messageReceived(String &topic, String &payload) {
+  const char* delimiter = ",";
+  String incomingMode = payload.substring(0,payload.indexOf(delimiter));
+  String colorValue   = payload.substring(incomingMode.length()+2,payload.length());
+  Serial.println("topic: " + topic);
+  Serial.println("payload: " + incomingMode);
+  
+  if(colorValue == "rainbow") {
+    rainbowOn = true;
+    colorR = 0;
+    colorG = 0;
+    colorB = 0;
+    Serial.println("color: " + colorValue);
+  }
+  else {
+    rainbowOn = false;  
+    // rainbow not used, get color values
+    String colorStringR = colorValue.substring(0, colorValue.indexOf(delimiter));
+    colorValue.remove(0,colorStringR.length()+2);
+    String colorStringG = colorValue.substring(0, colorValue.indexOf(delimiter));
+    colorValue.remove(0,colorStringG.length()+2);
+    String colorStringB = colorValue.substring(0, colorValue.indexOf(delimiter));
+    Serial.println("colorR: " + colorStringR);
+    Serial.println("colorG: " + colorStringG);
+    Serial.println("colorB: " + colorStringB);
+    colorR = colorStringR.toInt();
+    colorG = colorStringG.toInt();
+    colorB = colorStringB.toInt();
+  }
+  trigger(incomingMode.c_str());
+}
+
+void trigger(const char* event) {
+  if (strcmp(event, "fade") == 0){
+     currentMode = modeFade;
+  } else if (strcmp(event, "rain") == 0){
+     currentMode = modeRain;
+  } else if (strcmp(event, "sparkle") == 0){
+     currentMode = modeSparkle;
+  } else if (strcmp(event, "snake") == 0){
+   currentMode = modeSnake;
+  }
+}
+
 void loop() {
   switch(currentMode)
   {
-    case modeColorWipe:
-      Serial.print("colorWipe\n");
-      colorWipe(rings.Color(0, 0, 0, 255), 50); // White
+    case modeFade:
+      Serial.print("fade\n");
+      runFade(10);
       break;
-    case modeRainbow:
-      Serial.print("rainbow\n");
-      rainbow();
-      break;
-    case modeRain:
+     case modeRain:
       Serial.print("rain\n");
-      rain();
-      break;
-    case modeRainbowRain:
-      Serial.print("rainbow rain\n");
-      rainbowRain();
+      rain(25);
       break;
     case modeSparkle:
       Serial.print("sparkle\n");
-      sparkle();
+      sparkle(80);
       break;
     case modeSnake:
       Serial.print("snake\n");
-      snakeLoop();
-      break;
-    case modeRainbowSparkle:
-      Serial.print("rainbow sparkle\n");
-      rainbowSparkle();
+      snakeLoop(50);
       break;
     default:
       break;
@@ -108,99 +143,90 @@ void loop() {
   }
 }
 
-void messageReceived(String &topic, String &payload) {
-  Serial.println("incoming: " + topic + " - " + payload);
-  trigger(payload.c_str());
-}
-
-void trigger(const char* event) {
-  if(strcmp(event, "rainbow") == 0) {
-    Serial.printf("trigger event %s\n", event);
-    currentMode = modeRainbow;
-  } else if (strcmp(event, "colorWipe") == 0){
-     Serial.printf("trigger event %s\n", event);
-     currentMode = modeColorWipe;
-  } else if (strcmp(event, "rainbowRain") == 0){
-     Serial.printf("trigger event %s\n", event);
-     currentMode = modeRainbowRain;
-  } else if (strcmp(event, "rain") == 0){
-     Serial.printf("trigger event %s\n", event);
-     currentMode = modeRain;
-  } else if (strcmp(event, "sparkle") == 0){
-     Serial.printf("trigger event %s\n", event);
-     currentMode = modeSparkle;
-  } else if (strcmp(event, "snake") == 0){
-   Serial.printf("trigger event %s\n", event);
-   currentMode = modeSnake;
-  } else if (strcmp(event, "rainbowSparkle") == 0){
-    Serial.printf("trigger event %s\n", event);
-    currentMode = modeRainbowSparkle;
-  }
-}
-
-
 // Fill the dots one after the other with a color
-void colorWipe(uint32_t c, uint8_t wait) {
-  for(uint16_t i=0; i<rings.numPixels(); i++) {
-      rings.setPixelColor(i, c);
+void runFade(uint8_t wait) {
+  if(rainbowOn == true) {
+    uint16_t i, j;
+    for(j=0; j<256; j++) {
+      for(i=0; i<rings.numPixels(); i++) {
+        rings.setPixelColor(i, Wheel((i+j) & 255));
+      }
       rings.show();
       delay(wait);
+    }
+  }
+  else {
+    Index = 0;
+    Color1 = rings.Color(colorR, colorG, colorB);
+    Color2 = rings.Color(1,1,1);
+    while(Index+50 <= TotalSteps) {
+      fadeCycle();  // Fading darker
+      Index++;
+    }
+    while(Index > 50) {
+      fadeCycle();  // Fading brighter
+      Index--;
+    }
   }
 }
-
-// These are the pixels in order of animation-- 26 pixels in total:
-int sine[] = {
-   8, 9, 10, 11, 0, 1, 2, 3, 15, 16, 17, 18, 19, 49, 50, 51, 28,
-  29, 30, 31, 32, 33, 34, 35, 36, 37 };
-
- void snake() {
-    for(int i=0; i<26; i++) {
-    rings.setPixelColor(sine[i], rings.Color(75, 250, 100, 0)); // Draw 'head' pixel
-    rings.setPixelColor(sine[(i + 26 - 6) % 26], 0); // Erase 'tail'
-    rings.show();
-    delay(40);
+void fadeCycle() {
+  uint8_t red = ((Red(Color1) * (TotalSteps - Index)) + (Red(Color2) * Index)) / TotalSteps;
+  uint8_t green = ((Green(Color1) * (TotalSteps - Index)) + (Green(Color2) * Index)) / TotalSteps;
+  uint8_t blue = ((Blue(Color1) * (TotalSteps - Index)) + (Blue(Color2) * Index)) / TotalSteps;
+  for(uint16_t i=0; i<rings.numPixels(); i++) {
+    rings.setPixelColor(i, rings.Color(red, green, blue));        
   }
- }
+  rings.show();
+}
 
 // Array of pixels in order of animation - 52 in total:
 int sineLoop[] = {
    8, 9, 10, 11, 0, 1, 2, 3, 15, 16, 17, 18, 19, 49, 50, 51, 28,
   29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 
   46, 47, 48, 20, 21, 22, 23, 24, 25, 26, 27, 12, 13, 14, 4, 5, 6, 7};
- 
-void snakeLoop() {
-  for(int i=0; i<52; i++) {
-    rings.setPixelColor(sineLoop[i], 0); // Erase 'tail'
-    rings.setPixelColor(sineLoop[(i + 10) % 52], rings.Color(75, 250, 100, 0)); // Draw 'head' pixel
-    rings.show();
-    delay(60);
+  
+void snakeLoop(uint8_t wait) {
+  uint32_t color = rings.Color(colorR, colorG, colorB);
+  if (rainbowOn == true) {
+      for(uint16_t i=0; i<rings.numPixels(); i++) {
+        rings.setPixelColor(sineLoop[i], 0); // Erase 'tail'
+        rings.setPixelColor(sineLoop[(i + 10) % 52], Wheel(Index));
+        Index++;
+        if(Index > 255) {
+          Index = 0;
+        }
+        rings.show();
+        delay(wait);
+      }
+  }
+  else {
+    for(int i=0; i<52; i++) {
+      rings.setPixelColor(sineLoop[i], 0); // Erase 'tail'
+      rings.setPixelColor(sineLoop[(i + 10) % 52], color); // Draw 'head' pixel
+      rings.show();
+      delay(wait);
+    }
   }
 }
 
-// MR SPARKLE PROGRAM!!!!
-// Call createSparkle with white color
-void sparkle() {
-  createSparkle(rings.Color(255, 255, 255));
-}
-
-// Call createSparkle with random color
-void rainbowSparkle() {
-  createSparkle(rings.Color(random(255), random(255), random(255)));
-}
-
 // Draw 2 random leds, dim all leds, repeat
-void createSparkle(uint32_t color) {
-  delay(80);
+void sparkle(uint8_t wait) {
+  uint32_t color;
+  if (rainbowOn == true) {
+    color = rings.Color(random(255), random(255), random(255));
+  }
+  else {
+    color = rings.Color(colorR, colorG, colorB);
+  }
   rings.setPixelColor(random(52), color);
   rings.setPixelColor(random(52), color);
 
   for (int x=0; x<52; x++) {
     rings.setPixelColor(x, DimColor(rings.getPixelColor(x), .80));  
-
   }
   rings.show();
+  delay(wait);
 }
-
 uint32_t colors[52];
 int ring1[6] = {7,  8,  9, 10, 11,  0};
 int ring2[6] = {6,  5,  4,  3,  2,  1};
@@ -209,84 +235,37 @@ int ring4[8] = {17, 18, 19, 20, 21, 22, 23, 24};
 int ring5[12] = {40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51};
 int ring6[12] = {39, 38, 37, 36, 35, 34, 33, 32, 31, 30, 29, 28};
 
+// Moves a raindrop down 1 step
+void stepRaindrop(int arrayLength, int ringArray[], float fadeAmount) {
+  //first move any ON lights down one on each strip
+   for(int x=arrayLength; x > 0 ; x--) {
+    if (colors[ringArray[x-1]] != 0) {
+       colors[ringArray[x]] = colors[ringArray[x-1]];
+       colors[ringArray[x-1]] = DimColor(colors[ringArray[x-1]], fadeAmount);
+    }
+   }
+  // each row: special case, turn off all first lights
+  colors[ringArray[0]] = 0;  
+}
+
 // Rain Program
-void rain() {
-  delay(30);
-  uint32_t color = rings.Color(0, 0, 255, 0);
-  createRain(color);
-}
-
-// Rainbow Rain Program
-void rainbowRain() {
-  delay(25);
-  uint32_t color = rings.Color(random(255), random(255), random(255), 0);
-  createRain(color);
-}
-
-// Base Rain Program
-void createRain(uint32_t color) {
-
-   //first move any ON lights down one on each strip
-   for(int x=5; x > 0 ; x--) {
-    //if (colors[ring1[x-1]] != 0) {
-       colors[ring1[x]] = colors[ring1[x-1]];
-       colors[ring1[x-1]] = 0;
-     //}
-   }
-  // each row: special case turn off all first lights
-  colors[ring1[0]] = 0;
-
-  //first move any ON lights down one on each strip
-   for(int x=5; x > 0 ; x--) {
-    //if (colors[ring2[x-1]] != 0) {
-       colors[ring2[x]] = colors[ring2[x-1]];
-       colors[ring2[x-1]] = 0;
-     //}
-   }
-  // each row: special case turn off all first lights
-  colors[ring2[0]] = 0;
-
-  //first move any ON lights down one on each strip
-   for(int x=7; x > 0 ; x--) {
-    if (colors[ring3[x-1]] != 0) {
-       colors[ring3[x]] = colors[ring3[x-1]];
-       colors[ring3[x-1]] = DimColor(colors[ring3[x-1]], .40);
-     }
-   }
-  // each row: special case turn off all first lights
-  colors[ring3[0]] = 0;
-
-  //first move any ON lights down one on each strip
-   for(int x=7; x > 0 ; x--) {
-    if (colors[ring4[x-1]] != 0) {
-       colors[ring4[x]] = colors[ring4[x-1]];
-       colors[ring4[x-1]] = DimColor(colors[ring4[x-1]], .40);
-     }
-   }
-  // each row: special case turn off all first lights
-  colors[ring4[0]] = 0;
-
-  //first move any ON lights down one on each strip
-   for(int x=11; x > 0 ; x--) {
-    if (colors[ring5[x-1]] != 0) {
-       colors[ring5[x]] = colors[ring5[x-1]];
-       colors[ring5[x-1]] = DimColor(colors[ring5[x-1]], .50);
-     }
-   }
-  // each row: special case turn off all first lights
-  colors[ring5[0]] = 0;
-
-  //first move any ON lights down one on each strip
-   for(int x=11; x > 0 ; x--) {
-    if (colors[ring6[x-1]] != 0) {
-       colors[ring6[x]] = colors[ring6[x-1]];
-       colors[ring6[x-1]] = DimColor(colors[ring6[x-1]], .50);
-     }
-   }
-  // each row: special case turn off all first lights
-  colors[ring6[0]] = 0;
+void rain(uint8_t wait) {
+  uint32_t color;
+  if (rainbowOn == true) {
+    color = rings.Color(random(255), random(255), random(255));
+  }
+  else {
+    color = rings.Color(colorR, colorG, colorB);
+  }
+  stepRaindrop(5,ring1, .30);
+  stepRaindrop(5,ring2, .30);
+  stepRaindrop(7,ring3, .40);
+  stepRaindrop(7,ring4, .40);
+  stepRaindrop(13,ring5, .50);
+  stepRaindrop(13,ring6, .50);
   
   // turn on light at first position of random strip
+  // weighted a bit towards the bigger rings
   int randomOn = random(2);
   if (randomOn == 1) {
     int strip = random(10);
@@ -317,24 +296,11 @@ void createRain(uint32_t color) {
       break;
     }
   }
-
   updateRings();
+  delay(wait);
 }
 
-//Rainbow Program
-void rainbow() {
-  uint16_t i, j;
-
-  for(j=0; j<256; j++) {
-    for(i=0; i<rings.numPixels(); i++) {
-      rings.setPixelColor(i, Wheel((i+j) & 255));
-    }
-    rings.show();
-    delay(20);
-  }
-}
-
-// draw lights according to strips[][] array
+// udpate all lights according to colors[] array
 void updateRings() {
   for(int i=0; i<52; i++) {
     rings.setPixelColor(i, colors[i]);
@@ -357,29 +323,37 @@ uint32_t Wheel(int WheelPos) {
 }
 
 // Returns the Red component of a 32-bit color
-uint8_t Red(uint32_t color)
-{
+uint8_t Red(uint32_t color){
     return (color >> 16) & 0xFF;
 }
 
 // Returns the Green component of a 32-bit color
-uint8_t Green(uint32_t color)
-{
+uint8_t Green(uint32_t color){
     return (color >> 8) & 0xFF;
 }
 
 // Returns the Blue component of a 32-bit color
-uint8_t Blue(uint32_t color)
-{
+uint8_t Blue(uint32_t color){
     return color & 0xFF;
 }
 
 // Return color, dimmed by percentage
-uint32_t DimColor(uint32_t color, float dimPercent)
-{
+uint32_t DimColor(uint32_t color, float dimPercent){
   int red = Red(color) * dimPercent;
   int blue = Blue(color) * dimPercent;
   int green = Green(color) * dimPercent;
   uint32_t dimColor = rings.Color(red, green, blue);
   return dimColor;
 }
+
+//DEBUG - Determine pixel positions
+//rings.setPixelColor(0, rings.Color(255, 0, 0));
+//rings.setPixelColor(12, rings.Color(0, 255, 0));
+//rings.setPixelColor(23, rings.Color(0, 0, 255));
+//rings.setPixelColor(24, rings.Color(255, 0, 0));
+//rings.setPixelColor(31, rings.Color(0, 255, 0));
+//rings.setPixelColor(39, rings.Color(0, 0, 255));
+//rings.setPixelColor(40, rings.Color(255, 0, 0));
+//rings.setPixelColor(45, rings.Color(0, 255, 0));
+//rings.setPixelColor(51, rings.Color(0, 0, 255));
+//rings.show();
